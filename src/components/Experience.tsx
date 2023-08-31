3112914047323b0e891ea299f5ec0d4c76227a6d
Manoj-Kumar13 <manojkumar777.mk53@gmail.com>
2023-08-31 23:55:09 +0530

    added information about self and some styling

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import companyLogo from "../../public/1ds-logo.png";
 
 function Experience() {
   return (
-    <section>
-        <h3 className="text-3xl py-1 dark:text-white">Experience</h3>
-        <p>Software Engineer at 1Digitalstack</p>
+    <section className="px-10">
+      <h3 className="text-3xl py-1 text-center font-burtons dark:text-white">
+        Chronicles of My Journey
+      </h3>
+      <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
+        <Image
+          className="mx-auto"
+          alt="design"
+          src={companyLogo}
+          height={100}
+        />
+        <h3 className="text-xl font-medium pt-8 pb-2 font-mono text-black">
+          Software Engineer at{" "}
+          <span className="text-teal-600">1Digitalstack</span>
+        </h3>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Experience
+export default Experience;
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,74 +1,61 @@
-import Image from 'next/image'
+import Image from "next/image";
 import code from "../../public/code.png";
 import design from "../../public/design.png";
 import consulting from "../../public/consulting.png";
 
+const skillsData = [
+  {
+    title: "Coding Proficiencies",
+    image: design,
+    languages: ["Photoshop", "Illustrator", "Figma", "Indesign"],
+  },
+  {
+    title: "Frameworks and Libraries",
+    image: code,
+    languages: ["Photoshop", "Illustrator", "Figma", "Indesign"],
+  },
+  {
+    title: "Tools and Platforms",
+    image: consulting,
+    languages: ["Photoshop", "Illustrator", "Figma", "Indesign"],
+  },
+];
 
 function Skills() {
   return (
-    <section >
-    <div>
-      <h3 className="text-3xl py-1 text-center dark:text-white font-burtons">Enchanting Talents at Your Disposal</h3>
-      <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-        Since the beginning of my journey as a freelance designer and
-        developer, I've done remote work for
-        <span className="text-teal-500"> agencies </span>
-        consulted for <span className="text-teal-500">startups </span>
-        and collaborated with talanted people to create digital products
-        for both business and consumer use.
-      </p>
-      <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-        I offer from a wide range of services, including brand design,
-        programming and teaching.
-      </p>
-    </div>
-    <div className="lg:flex gap-10">
-      <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
-        <Image alt="design" src={design} width={100} height={100} />
-        <h3 className="text-lg font-medium pt-8 pb-2  text-black">
-          Programming Languages
+    <section className="px-10 mb-10 pb-10">
+      <div>
+        <h3 className="text-3xl py-1 text-center dark:text-white font-burtons">
+          Enchanting Talents at Your Disposal
         </h3>
-        <p className="py-2">
-          Creating elegant designs suited for your needs following core
-          design theory.
+        <p className="text-md py-2 px-16 leading-8 text-[#afafbf] font-mono text-xl ">
+          Step into a world where a diverse tapestry of enchanting talents
+          stands poised at your service, each thread meticulously woven to craft
+          exceptional experiences and weave the extraordinary
         </p>
-        <h4 className="py-4 text-teal-600">Languages</h4>
-        <p className="text-gray-800 py-1">Photoshop</p>
-        <p className="text-gray-800 py-1">Illustrator</p>
-        <p className="text-gray-800 py-1">Figma</p>
-        <p className="text-gray-800 py-1">Indesign</p>
       </div>
-      <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-        <Image alt="code" src={code} width={100} height={100} />
-        <h3 className="text-lg font-medium pt-8 pb-2 text-black">
-          Frameworks and Libraries
-        </h3>
-        <p className="py-2">
-          Do you have an idea for your next great website? Let's make it a
-          reality.
-        </p>
-        <h4 className="py-4 text-teal-600">Frameworks and libraries</h4>
-        <p className="text-gray-800 py-1">Photoshop</p>
-        <p className="text-gray-800 py-1">Illustrator</p>
-        <p className="text-gray-800 py-1">Figma</p>
-        <p className="text-gray-800 py-1">Indesign</p>
+      <div className="lg:flex gap-10">
+        {skillsData.map((skill) => {
+          return (
+            <div className="text-center shadow-lg py-10 rounded-xl my-10  dark:bg-white flex-1">
+              <Image alt="design" src={skill.image} width={100} height={100} />
+              <h3 className="text-lg font-medium pt-8 pb-2  text-black">
+                {skill.title}
+              </h3>
+              <p className="py-2 text-black">
+                Creating elegant designs suited for your needs following core
+                design theory.
+              </p>
+              <h4 className="py-4 text-teal-600">Languages</h4>
+              {skill.languages.map((language) => {
+                return <p className="text-gray-800 py-1">{language}</p>;
+              })}
+            </div>
+          );
+        })}
       </div>
-      <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-        <Image alt='consulting' src={consulting} width={100} height={100} />
-        <h3 className="text-lg font-medium pt-8 pb-2 text-black">Tools and Platforms</h3>
-        <p className="py-2">
-          Are you interested in feedback for your current project? I can
-          give you tips and tricks to level it up.
-        </p>
-        <h4 className="py-4 text-teal-600">Tools</h4>
-        <p className="text-gray-800 py-1">Photoshop</p>
-        <p className="text-gray-800 py-1">Illustrator</p>
-        <p className="text-gray-800 py-1">Figma</p>
-        <p className="text-gray-800 py-1">Indesign</p>
-      </div>
-    </div>
-  </section>
-  )
+    </section>
+  );
 }
 
-export default Skills
+export default Skills;
